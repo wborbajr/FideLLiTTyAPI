3be61a5bc731b0023fea39ffa7c20c2cb3f16b72
Waldir Borba Junior <wborbajr@gmail.com>
2020-05-15 18:10:03 -0300

    feat: config

--- fidellittyapi/core/config.py
+++ fidellittyapi/core/config.py
@@ -1,7 +1,6 @@
 import os
 
 from dynaconf import settings
-from starlette.datastructures import CommaSeparatedStrings, Secret
 from databases import DatabaseURL
 
 API_V1_STR = "/api"
@@ -9,20 +8,21 @@ API_V1_STR = "/api"
 JWT_TOKEN_PREFIX = "Token"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
 
-MAX_CONNECTIONS_COUNT = int(settings.MAX_CONNECTIONS_COUNT, 10)
-MIN_CONNECTIONS_COUNT = int(settings.MIN_CONNECTIONS_COUNT, 10)
-SECRET_KEY = Secret(settings.SECRET_KEY, "secret key for project")
+MAX_CONNECTIONS_COUNT = settings.MAX_CONNECTIONS_COUNT
+MIN_CONNECTIONS_COUNT = settings.MIN_CONNECTIONS_COUNT
+SECRET_KEY = settings.SECRET_KEY
 
 PROJECT_NAME = settings.PROJECT_NAME, "FastAPI example application"
-ALLOWED_HOSTS = CommaSeparatedStrings(settings.ALLOWED_HOSTS, "")
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
-MONGODB_URL = settings.MONGODB_URL, ""  # deploying without docker-compose
+MONGODB_URL = settings.MONGODB_URL  # deploying without docker-compose
+
 if not MONGODB_URL:
-    MONGO_HOST = settings.MONGO_HOST, "localhost"
-    MONGO_PORT = int(settings.MONGO_PORT, 27017)
-    MONGO_USER = settings.MONGO_USER, "admin"
-    MONGO_PASS = settings.MONGO_PASSWORD, "markqiu"
-    MONGO_DB = settings.MONGO_DB, "fastapi"
+    MONGO_HOST = settings.MONGO_HOST
+    MONGO_PORT = settings.MONGO_PORT
+    MONGO_USER = settings.MONGO_USER
+    MONGO_PASS = settings.MONGO_PASSWORD
+    MONGO_DB = settings.MONGO_DB
 
     MONGODB_URL = DatabaseURL(
         f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
